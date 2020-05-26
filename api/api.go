d48468ed46966c1516666208afb21f1a0415f436
capricorn <capricorn@basebandit.org>
2020-05-26 11:59:27 +0300

    Add walk function to print out the routes

--- api/api.go
+++ api/api.go
@@ -5,6 +5,7 @@ import (
 	"log"
 	"net/http"
 	"runtime/debug"
+	"strings"
 	"time"
 
 	"github.com/go-chi/chi"
@@ -32,6 +33,16 @@ func New(enableCors bool, store store.Store, logger *log.Logger) (*chi.Mux, erro
 	r.Get("/ping", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("pong"))
 	})
+
+	walkFunc := func(method string, route string, handler http.Handler, middlewares ...func(http.Handler) http.Handler) error {
+		route = strings.Replace(route, "/*/", "/", -1)
+		fmt.Printf("%s %s\n", method, route)
+		return nil
+	}
+
+	if err := chi.Walk(r, walkFunc); err != nil {
+		fmt.Printf("Logging err:%s\n", err.Error())
+	}
 
 	return r, nil
 }
